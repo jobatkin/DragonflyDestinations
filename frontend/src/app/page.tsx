2394276dd4afb85bcc278d8e7d6fc6fd0eead864
jobatkin <jobatkin@gmail.com>
2024-04-28 16:00:50 +1000

    get real random country from backend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,32 +5,33 @@ import CountryCard from "@/components/CountryCard";
 
 // Save as app/posts/page.jsx and copy layout.jsx from /about
 async function getRandomCountries(limit: number) {
-    //const res = await fetch("/api/countries/random/?_limit=" + limit);
+    const res = await fetch(process.env.SERVER + "/api/countries/random/?limit=" + limit);
 
-    //if (!res.ok) {
+    if (!res.ok) {
         // Recommendation: handle errors
         // This will activate the closest `error.js` Error Boundary
-    //    throw new Error("Failed to fetch countries");
-    //}
-    //return res.json();
+       throw new Error("Failed to fetch countries");
+    }
+    return res.json();
 
-    // temp data hardcoded for testing
+    //temp data hardcoded for testing
 
-    return [
-      {
-        name: 'Australia',
-        capital: 'Canberra',
-        region: 'Oceania',
-        subregion: 'Australia and New Zealand',
-        flagImg: 'https://flagcdn.com/au.svg',
-        population: 25687041,
-        area: 7692024
-      }
-    ]
+    // return [
+    //   {
+    //     name: 'Australia',
+    //     capital: 'Canberra',
+    //     region: 'Oceania',
+    //     subregion: 'Australia and New Zealand',
+    //     flagImg: 'https://flagcdn.com/au.svg',
+    //     population: 25687041,
+    //     area: 7692024
+    //   }
+    // ]
 }
 
 export default async function Home() {
   const countries = await getRandomCountries(1);
+  const country = countries.data[0];
 
   return (
     <main className={styles.main}>
@@ -49,10 +50,10 @@ export default async function Home() {
             <Grid item md={1} sx={{display: {xs: "none", md: "flex"}}}></Grid>
             <Grid item md={5} xs={12}>
               <CountryCard 
-                name={countries[0].name} capital={countries[0].capital} 
-                region={countries[0].region} subregion={countries[0].subregion} 
-                flagImg={countries[0].flagImg}
-                population={countries[0].population} area={countries[0].area}/>
+                name={country.name} capital={country.capital} 
+                region={country.region} subregion={country.subregion} 
+                flagImg={country.flag.svgLink}
+                population={country.population} area={country.area}/>
             </Grid>
           </Grid>
         </Container>
