e4ef7200deee4ef540ec0c81a7b1e4d88c378071
jobatkin <jobatkin@gmail.com>
2024-05-14 10:29:51 +1000

    added new home sections

---
+++ frontend/src/components/CalloutModule.tsx
@@ -0,0 +1,43 @@
+import { Box, Button, ButtonGroup, Typography } from "@mui/material";
+import Link from "next/link";
+
+export interface CalloutModuleProps {
+    title: string,
+    children: React.ReactNode,
+    button1?: CalloutButtonProps,
+    button2?: CalloutButtonProps,
+    shadow?: Boolean
+}
+
+export interface CalloutButtonProps {
+    text: string,
+    link: string,
+    color?: string
+}
+
+function CalloutModule({title, children, button1, button2, shadow = false}: CalloutModuleProps) {
+    const boxStyles = { display: 'flex', flexDirection: 'column', boxShadow: 'none', padding: {xs: '0.5em', lg: '2em'} };
+    if (shadow) boxStyles.boxShadow = '2px 2px 10px 5px rgba(0,0,0,0.5)';
+    
+    const hasButtons = button1 || button2;
+    const textStyles = hasButtons ? { mb: '1em' } : {};
+
+    return (
+        <Box sx={boxStyles} className="CalloutModule">
+            <Typography variant="h3" gutterBottom>
+                {title}
+            </Typography>
+            <Typography component="div" sx={textStyles}>
+                {children}
+            </Typography>
+            {hasButtons ? 
+                <ButtonGroup>
+                    {button1 && <Button component={Link} href={button1.link} size="large" variant="contained">{button1.text}</Button>}
+                    {button2 && <Button component={Link} href={button2.link} size="large" variant="outlined">{button2.text}</Button>}
+                </ButtonGroup>
+            : null}
+        </Box>
+    )
+}
+
+export default CalloutModule;
---
+++ frontend/src/components/CountryCard.tsx
@@ -0,0 +1,39 @@
+import { CardContent, Card, CardMedia, Typography, Grid } from "@mui/material";
+import { Country } from "@/types";
+
+export interface CountryCardProps extends Country {
+    flagImg: string,
+    colour?: string
+}
+
+function CountryCard(props: CountryCardProps) {
+    const colour = props.colour ? props.colour : 'primary';
+
+    return (
+        <Card className="CountryCard" sx={{ backgroundColor: `${colour}.main`, color: `${colour}.contrastText`, mx: {xs: '0.5em', lg:'2em'} }}>
+            <CardMedia
+                sx={{ height: 140 }}
+                image={props.flagImg}
+                title={props.name}
+            />            
+            <CardContent>
+                <Typography gutterBottom variant="h3">
+                    {props.name}
+                </Typography>
+                <Typography gutterBottom variant="h5" color="text.secondary">
+                    Where? {props.subregion}, {props.region}
+                </Typography>      
+                <Grid container>
+                    <Grid item xs={6}>
+                        <Typography variant="body2">Population: {props.population.toLocaleString()}</Typography>
+                    </Grid>
+                    <Grid item xs={6}>
+                        <Typography variant="body2">Area: {props.area.toLocaleString()}km<sup>2</sup></Typography>
+                    </Grid>                    
+                </Grid>          
+            </CardContent>
+        </Card>
+    )
+}
+
+export default CountryCard
