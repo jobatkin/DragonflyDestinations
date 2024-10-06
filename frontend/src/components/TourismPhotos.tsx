d5a5c990b52a4e4806f8c6ffc661f76052ef376f
jobatkin <jobatkin@gmail.com>
2024-10-06 15:15:07 +1000

    added comments

--- frontend/src/components/TourismPhotos.tsx
+++ frontend/src/components/TourismPhotos.tsx
@@ -1,6 +1,7 @@
 import { ImageList, ImageListItem } from "@mui/material";
 import Image from "next/image";
 
+// display a list of photos for a country, displayed in a grid
 function TourismPhotos({photos, country}: {photos: string[], country: string}) {
     return (
         <ImageList sx={{ width: '100%', my: 4, gridTemplateColumns: 'repeat(auto-fill, minmax(280px, 1fr)) !important' }} cols={5} gap={16} rowHeight={250}>
--- frontend/src/components/TourismReasons.tsx
+++ frontend/src/components/TourismReasons.tsx
@@ -1,6 +1,7 @@
 import WhereToVoteIcon from "@mui/icons-material/WhereToVote";
 import TourismItems from "./TourismItems";
 
+// display a list of reasons for tourists to visit a country, using predefined icons and heading
 function TourismReasons({reasons}: {reasons: string[]}) {
     return (
         <TourismItems items={reasons} colour="primary" icon={<WhereToVoteIcon/>} heading="Reasons to visit:"/>
